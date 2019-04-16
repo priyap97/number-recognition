af57fce1f557e8b2ea975d70a2192991f50acdbb
Kyle Chen <Oryon.KC@gmail.com>
2019-04-16 10:39:52 -0400

    Add files via upload

--- number_recognition.py
+++ number_recognition.py
@@ -1,1 +1,17 @@
-# Main driver for project
+import nengo
+import numpy as np
+import matplotlib.pyplot as plt
+
+model = nengo.Network()
+
+with model:
+	input_node = nengo.Node([0]*784)
+	ens = nengo.Ensemble(1000,1)
+	output = nengo.Node(size_in=1000)
+	output_probe = nengo.Probe(output)
+
+	nengo.Connection(input_node, ens.neurons, synapse=None, transform=nengo_dl.dists.Glorot())
+	nengo.Connection(ens.neurons, output, synapse=None, transform=nengo_dl.dists.Glorot())
+
+with nengo_dl.Simulator(net) as sim:
+	sim.train({input_node: spike_train}, {output_p, spike_train})
