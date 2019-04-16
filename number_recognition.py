94315eb403cffac2ed8bd1ea349a3881b3667ce9
Kyle Chen <Oryon.KC@gmail.com>
2019-04-16 18:08:38 -0400

    Update number_recognition.py

--- number_recognition.py
+++ number_recognition.py
@@ -10,8 +10,11 @@ with model:
 	output = nengo.Node(output=callable, size_in=1, size_out=10)
 	output_probe = nengo.Probe(output)
 
-	nengo.Connection(input_node, ens.neurons, synapse=None, transform=nengo_dl.dists.Glorot())
-	nengo.Connection(ens.neurons, output, synapse=None, transform=nengo_dl.dists.Glorot())
+	nengo.Connection(input_node, ens.neurons)
+	nengo.Connection(ens.neurons, output)
+	
+	#nengo.Connection(input_node, ens.neurons, synapse=None, transform=nengo_dl.dists.Glorot())
+	#nengo.Connection(ens.neurons, output, synapse=None, transform=nengo_dl.dists.Glorot())
 
-with nengo_dl.Simulator(net) as sim:
-	sim.train({input_node: spike_train}, {output_p, spike_train})
+#with nengo_dl.Simulator(net) as sim:
+	#sim.train({input_node: spike_train}, {output_p, spike_train})
